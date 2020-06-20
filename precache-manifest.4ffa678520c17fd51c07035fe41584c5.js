9c615c244cd5c32af9c13e8627d14837b021b682
Sebastian Guerrero Nuñez <sebastianguerreronunez@Sebastians-MacBook-Pro.local>
2020-06-19 21:40:06 -0400

    Updates

---
+++ precache-manifest.4ffa678520c17fd51c07035fe41584c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd072ad842be5f69dddf389d17e93403",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "26a3f5484f50b549df5d",
+    "url": "/monster-rolodex/static/css/main.f9f3e9c3.chunk.css"
+  },
+  {
+    "revision": "6725e1c9464ab25b8337",
+    "url": "/monster-rolodex/static/js/2.1d6c4f92.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/monster-rolodex/static/js/2.1d6c4f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26a3f5484f50b549df5d",
+    "url": "/monster-rolodex/static/js/main.d539127d.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
